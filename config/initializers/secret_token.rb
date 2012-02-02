ab52c747d4fead1e1cc254e9d15b48f84ac4c191
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-02-01 16:32:13 -0800

    added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingCrud::Application.config.secret_token = '7be5f176edb8581a709fa9c11c9f4cf87fdd5416b8bda894e76c6d4ac240c4fd8ab4db542fc2f030d3f70f96c4fe7a4a208d9803998b8669540c341848770015'
