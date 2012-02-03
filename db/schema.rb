33933465265b19e24863208e9d85bf1dc0d03515
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-02-03 12:12:56 -0800

    created scaffold for movie

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120202003444) do
+ActiveRecord::Schema.define(:version => 20120203200059) do
+
+  create_table "movies", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
