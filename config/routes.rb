0d281105c21b9b4148dba6dd89b548e39ee81f4b
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-02-02 19:32:05 -0800

    added route for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TrainingCrud::Application.routes.draw do
   resources :users
+  match "/users" => "users#search", :as => "search_user", :via => "get"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
