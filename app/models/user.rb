a6a5cdcf9fc88d6dba751414ed10c3ce2619461b
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-02-02 19:31:28 -0800

    added condition for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   before_create :record_available
 
+  def self.search(search)
+    if search
+      find(:all,:conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 
 
 
